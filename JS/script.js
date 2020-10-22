caef2864f754ba9195564c3e1e2dd8cf2854dfd3
Adrian <60756264+adrian-tagama@users.noreply.github.com>
2020-10-22 01:35:53 -0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,6 @@ function fade() {
     }
     setTimeout(fade, 3000);
 
-$("#down-arrow").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#work").offset().top
-    }, 2000);
-});
-
 const menuButton = document.getElementsByClassName('menu-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
